b7e43c416cdcf0fd3542fb1d141e8527eaa8f7f0
mdimran1409036 <mdimran1409036@gmail.com>
2021-11-23 18:07:43 +0600

    header added

---
+++ src/Pages/Home/Header/Header.js
@@ -0,0 +1,46 @@
+import Button from '@restart/ui/esm/Button';
+import React from 'react';
+import { Container, Form, FormControl, InputGroup, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar style={{ backgroundColor: "var(--bgMain)" }} expand="lg" variant="dark">
+            <Container fluid>
+                <Navbar.Brand href="#" style={{ display: 'block', backgroundColor: "var(--bgSecondary)", padding: "5px 10px" }}>Ryans Clone</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Container >
+                    <Form className="d-flex w-75 mx-auto p-2">
+                        <InputGroup>
+                            <FormControl
+                                placeholder="Search"
+                                aria-label="Recipient's username"
+                                aria-describedby="basic-addon2"
+                            />
+                            <Button id="button-addon2">
+                                Search
+                            </Button>
+                        </InputGroup>
+                    </Form>
+                </Container>
+                <Navbar.Collapse id="responsive-navbar-nav" >
+                    <Nav
+                        className="ms-auto my-2 pb-2 my-lg-0"
+                    >
+                        <NavDropdown title="All Products" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Brands</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Laptos</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Monitor</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Tablets</NavDropdown.Item>
+
+                        </NavDropdown>
+                        <Nav.Link href="#action1" className="text-white">Cart</Nav.Link>
+                        <Nav.Link href="#action2" className="text-white">Account</Nav.Link>
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
